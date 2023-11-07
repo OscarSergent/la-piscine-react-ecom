91fd15608f797a21eac8bae51248852065f9c105
OscarSergent <150020702+OscarSergent@users.noreply.github.com>
2023-11-07 15:19:48 +0100

    Add files via upload

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+function Footer () {
+    return (
+        <footer>
+            <p>ceci est un footer</p>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/Products.js
@@ -0,0 +1,22 @@
+function Products () {
+    return (
+        <main>
+            <article>
+                <h2>produit1</h2>
+                <p>truc</p>
+            </article>
+            
+            <article>
+                <h2>produit2</h2>
+                <p>truc</p>
+            </article>
+            
+            <article>
+                <h2>produit3</h2>
+                <p>truc</p>
+            </article>
+        </main>
+    )
+}
+
+export default Products;
